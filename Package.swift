18fedefde7fbae7d46d401023c14fcdd108f5ce3
Taylor Franklin <tfranklin@us.ibm.com>
2017-01-23 17:27:09 -0600

    set specific package versions

--- Package.swift
+++ Package.swift
@@ -7,13 +7,13 @@ var package = Package(
         ],
     dependencies: [
         // For MongoDB Documents
-        .Package(url: "https://github.com/OpenKitten/BSON.git", majorVersion: 4),
+        .Package(url: "https://github.com/OpenKitten/BSON.git", Version(4,0,0)),
 
         // Authentication
         .Package(url: "https://github.com/OpenKitten/CryptoKitten.git", Version(0,0,2)),
         
         // Provides sockets
-        .Package(url: "https://github.com/vapor/socks.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/socks.git", Version(1,2,2)),
 
         // SSL
 //        .Package(url: "https://github.com/vapor/tls.git", majorVersion: 1),
