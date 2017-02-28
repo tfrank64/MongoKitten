b267053b268b31560e82a4a728f1e5477681142b
Taylor Franklin <tfranklin@us.ibm.com>
2017-02-28 17:24:14 -0600

    reverting package changes

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ var package = Package(
         ],
     dependencies: [
         // For MongoDB Documents
-        .Package(url: "https://github.com/OpenKitten/BSON.git", "4.0.1"),
+    .Package(url: "https://github.com/OpenKitten/BSON.git", majorVersion: 4, minor: 0),
 
         // Authentication
         .Package(url: "https://github.com/OpenKitten/CryptoKitten.git", Version(0,0,2)),
@@ -17,7 +17,7 @@ var package = Package(
 
         // SSL
 //        .Package(url: "https://github.com/vapor/tls.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", "0.12.16"),
+        .Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0, minor: 12),
 
         // Logging
         .Package(url: "https://github.com/OpenKitten/LogKitten.git", majorVersion: 0, minor: 3),
